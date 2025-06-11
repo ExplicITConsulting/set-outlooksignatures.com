4246d2ab1cc8d39d6e0a79c1874be5904ba8d402
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-06-11 11:35:51 +0200

    fix: streamline tracking code by removing unnecessary cookie consent methods

--- _plugins/inject_tracking_code.rb
+++ _plugins/inject_tracking_code.rb
@@ -3,17 +3,13 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
     tracking_code = <<~HTML
       <script>
         var _paq = window._paq = window._paq || [];
+        _paq.push(["setRequestMethod", "POST"]);
 
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(["setRequestMethod", "POST"]);
-        _paq.push(["requireCookieConsent"]);
-        _paq.push(["disableCookies"]);
-        _paq.push(["deleteCookies"]);
-        _paq.push(["enableHeartBeatTimer"]);
-        //_paq.push(["disableAlwaysUseSendBeacon"]);
         _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
         _paq.push(["setCookieDomain", "*.set-outlooksignatures.com"]);
         _paq.push(["setDomains", ["*.set-outlooksignatures.com"]]);
+        _paq.push(["disableCookies"]);
         _paq.push(['trackPageView']);
         _paq.push(['enableLinkTracking']);
 
