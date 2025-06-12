159a605956e469f710d059e2bd83689d70aeae22
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-06-12 17:39:25 +0200

    fix: reorder tracking code initialization for consistency in inject_tracking_code plugin

--- _plugins/inject_tracking_code.rb
+++ _plugins/inject_tracking_code.rb
@@ -3,13 +3,13 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
     tracking_code = <<~HTML
       <script>
         var _paq = window._paq = window._paq || [];
-        _paq.push(["setRequestMethod", "POST"]);
-
         /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["setRequestMethod", "POST"]);
         _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
         _paq.push(["setCookieDomain", "*.set-outlooksignatures.com"]);
         _paq.push(["setDomains", ["*.set-outlooksignatures.com"]]);
         _paq.push(["disableCookies"]);
+        _paq.push(["disableQueueRequest"]);
         _paq.push(['enableHeartBeatTimer']);
         _paq.push(["disableAlwaysUseSendBeacon"]);
         _paq.push(['setLinkClasses', "mtrcs-external-link"]);
