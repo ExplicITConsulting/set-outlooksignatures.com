9c8c2e06ef80869062aa6eed6ed2d82c91027006
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-06-28 07:45:11 +0200

    feat: remove unnecessary tracking script code for improved performance

--- _plugins/inject_custom_js.rb
+++ _plugins/inject_custom_js.rb
@@ -128,34 +128,6 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
       </script>
 
       <script>
-        var _paq = window._paq = window._paq || [];
-        _paq.push(["setRequestMethod", "POST"]);
-        _paq.push(["disableCookies"]);
-        _paq.push(["deleteCookies"]);
-        _paq.push(["disableAlwaysUseSendBeacon"]);
-        _paq.push(["disableQueueRequest"]);
-        _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-        _paq.push(["setCookieDomain", "*.set-outlooksignatures.com"]);
-        _paq.push(["setDomains", ["*.set-outlooksignatures.com"]]);
-        _paq.push(['enableHeartBeatTimer']);
-        _paq.push(['setLinkClasses', "mtrcs-external-link"]);
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-
-        (function () {
-          var u = "//mtrcs.explicitconsulting.at/";
-          _paq.push(["setTrackerUrl", u + "poop.php"]);
-          _paq.push(["setSiteId", "1"]);
-          var d = document, g = d.createElement("script"), s = d.getElementsByTagName("script")[0];
-          g.type = "text/javascript"; g.async = true; g.defer = true; g.src = u + "poop.js"; s.parentNode.insertBefore(g, s);
-        })();
-      </script>
-
-      <noscript>
-        <p><img referrerpolicy="no-referrer-when-downgrade" src="//mtrcs.explicitconsulting.at/poop.php?idsite=1&amp;rec=1" style="border:0;" alt="" /></p>
-      </noscript>
-
-      <script>
         document.addEventListener("DOMContentLoaded", function () {
           const links = document.querySelectorAll("a[href]");
           links.forEach(link => {
