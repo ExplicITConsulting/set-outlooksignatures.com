75bdae6df473a574a11ddb5ab358aca62f8e8b30
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-08-14 14:04:36 +0200

    trackVisibleContentImpressions

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -4,15 +4,32 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   # Only process HTML files
   next unless doc.output_ext == '.html'
 
+  # First, handle headings without IDs
   id_counter = 1
+  regex_no_id = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>(.*?)<\/\1>/i
 
-  # This regex matches <h1> ... <h6> tags that do NOT have an id= attribute
-  # and captures the start tag (group 1) and the rest of the tag name & attributes (group 2)
-  regex = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>/i
-
-  doc.output = doc.output.gsub(regex) do
+  doc.output = doc.output.gsub(regex_no_id) do
     tag_name = Regexp.last_match(1)
     rest = Regexp.last_match(2)
-    %Q{<#{tag_name} id="heading-#{id_counter}"#{rest}>}.tap { id_counter += 1 }
+    content = Regexp.last_match(3)
+
+    new_tag = %Q{<#{tag_name}#{rest} id="heading-#{id_counter}">#{content}</#{tag_name}>}
+    id_counter += 1
+    new_tag
+  end
+
+  # Second, add Matomo tracking attribute to all headings
+  regex_all_headings = /<(h[1-6])([^>]*)>(.*?)<\/\1>/i
+
+  doc.output = doc.output.gsub(regex_all_headings) do
+    tag_name = Regexp.last_match(1)
+    attributes = Regexp.last_match(2)
+    content = Regexp.last_match(3)
+
+    full_url = doc.site.config['url'].to_s + doc.url.to_s
+    sanitized_content = content.strip.gsub(/<[^>]*>/, '')
+    matomo_name = %Q{ data-matomo-content-name="#{full_url} (#{sanitized_content})"}
+
+    %Q{<#{tag_name}#{attributes}#{matomo_name}>#{content}</#{tag_name}>}
   end
 end
