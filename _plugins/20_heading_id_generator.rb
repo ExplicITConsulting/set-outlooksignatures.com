03ead4b40ba7f98880237b1f0328c2b037a4ea13
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-08-14 13:45:40 +0200

    trackVisibleContentImpressions

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -4,28 +4,39 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   # Only process HTML files
   next unless doc.output_ext == '.html'
 
+  # First, handle headings without IDs using the original logic
   id_counter = 1
-  
-  # This regex matches <h1>...<h6> tags that do NOT have an id= attribute
-  # It captures the tag name (group 1), attributes (group 2), and content (group 3)
-  regex = /<(h[1-6])(\b[^>]*?)>(.*?)<\/\1>/i
+  regex_no_id = /<(h[1-6])\b(?![^>]*\bid=)([^>]*?)>(.*?)<\/\1>/i
 
-  doc.output = doc.output.gsub(regex) do
+  doc.output = doc.output.gsub(regex_no_id) do |match|
+    tag_name = Regexp.last_match(1)
+    rest = Regexp.last_match(2)
+    content = Regexp.last_match(3)
+    
+    # Create the new heading with an ID
+    new_tag = %Q{<#{tag_name}#{rest} id="heading-#{id_counter}">#{content}</#{tag_name}>}
+    id_counter += 1
+    new_tag
+  end
+
+  # Second, handle all headings to add the Matomo attribute
+  regex_all_headings = /<(h[1-6])([^>]*?)>(.*?)<\/\1>/i
+
+  doc.output = doc.output.gsub(regex_all_headings) do |match|
     tag_name = Regexp.last_match(1)
     attributes = Regexp.last_match(2)
     content = Regexp.last_match(3)
-    
-    # Get the full URL of the page
+
+    # Get the full URL of the page.
     full_url = doc.site.config['url'] + doc.url
-    
-    # Create the data-matomo-content-name attribute
-    matomo_name = "#{full_url} (#{content.strip.gsub(/<[^>]*>/, '')})"
-    
-    # Check if an id attribute already exists to avoid duplication
-    id_attribute = attributes.match(/\bid=/) ? "" : %Q{ id="heading-#{id_counter}"}
-    
-    # Build the new tag with the added attributes
-    new_tag = %Q{<#{tag_name}#{attributes}#{id_attribute} data-matomo-content-name="#{matomo_name}">#{content}</#{tag_name}>}.tap { id_counter += 1 }
-    new_tag
+
+    # Sanitize the heading content for a Matomo name.
+    sanitized_content = content.strip.gsub(/<[^>]*>/, '')
+
+    # Create the data-matomo-content-name attribute.
+    matomo_name = %Q{ data-matomo-content-name="#{full_url} (#{sanitized_content})"}
+
+    # Reconstruct the heading tag with the new attribute.
+    %Q{<#{tag_name}#{attributes}#{matomo_name}>#{content}</#{tag_name}>}
   end
 end
