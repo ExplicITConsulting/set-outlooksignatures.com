036e301bfa5776ad216d0765abe2a1408a26f2a7
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-08-14 14:16:47 +0200

    trackVisibleContentImpressions

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -1,23 +1,39 @@
-# _plugins/add_heading_ids_safe.rb
-
 Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   # Only process HTML files
   next unless doc.output_ext == '.html'
 
-  # First, make sure all headings have an id attribute
+  site_url = doc.site.config['url'].to_s.chomp('/')
+  page_url = doc.url.to_s
+  full_page_url = "#{site_url}#{page_url}"
+
   id_counter = 1
 
-  # This regex matches <h1> ... <h6> tags that do NOT have an id attribute
-  # and captures the start tag (group 1) and the rest of the tag name & attributes (group 2)
-  regex = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>/i
-
-  doc.output = doc.output.gsub(regex) do
+  # First, add missing id attributes to headings
+  regex_id = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>/i
+  doc.output = doc.output.gsub(regex_id) do
     tag_name = Regexp.last_match(1)
     rest = Regexp.last_match(2)
     %Q{<#{tag_name} id="heading-#{id_counter}"#{rest}>}.tap { id_counter += 1 }
   end
 
+  # Second, add missing data-matomo-content-name attributes using the heading's id
+  regex_matomo = /<(h[1-6])([^>]*?)\s*(?=>)/i
+  doc.output = doc.output.gsub(regex_matomo) do
+    tag_name = Regexp.last_match(1)
+    rest = Regexp.last_match(2)
 
-  # Second, make sure that all headings have an data-matomo-content-name attribute
+    # Extract existing id
+    id_match = rest.match(/\bid="'["']/)
+    heading_id = id_match ? id_match[1] : nil
 
+    # Skip if no id found (shouldn't happen if first part ran correctly)
+    next "<#{tag_name}#{rest}>" unless heading_id
+
+    # Add data-matomo-content-name if missing
+    if rest =~ /\bdata-matomo-content-name=/
+      "<#{tag_name}#{rest}>"
+    else
+      %Q{<#{tag_name}#{rest} data-matomo-content-name="#{full_page_url}##{heading_id}">}
+    end
+  end
 end
