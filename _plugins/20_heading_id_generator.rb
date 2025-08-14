754841df5f5b371c39b99308d850ae83c0826a00
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-08-14 13:40:41 +0200

    trackVisibleContentImpressions

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -5,14 +5,27 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   next unless doc.output_ext == '.html'
 
   id_counter = 1
-
-  # This regex matches <h1> ... <h6> tags that do NOT have an id= attribute
-  # and captures the start tag (group 1) and the rest of the tag name & attributes (group 2)
-  regex = /<(h[1-6])\b(?![^>]*\bid=)([^>]*)>/i
+  
+  # This regex matches <h1>...<h6> tags that do NOT have an id= attribute
+  # It captures the tag name (group 1), attributes (group 2), and content (group 3)
+  regex = /<(h[1-6])(\b[^>]*?)>(.*?)<\/\1>/i
 
   doc.output = doc.output.gsub(regex) do
     tag_name = Regexp.last_match(1)
-    rest = Regexp.last_match(2)
-    %Q{<#{tag_name} id="heading-#{id_counter}"#{rest}>}.tap { id_counter += 1 }
+    attributes = Regexp.last_match(2)
+    content = Regexp.last_match(3)
+    
+    # Get the full URL of the page
+    full_url = doc.site.config['url'] + doc.url
+    
+    # Create the data-matomo-content-name attribute
+    matomo_name = "#{full_url} (#{content.strip.gsub(/<[^>]*>/, '')})"
+    
+    # Check if an id attribute already exists to avoid duplication
+    id_attribute = attributes.match(/\bid=/) ? "" : %Q{ id="heading-#{id_counter}"}
+    
+    # Build the new tag with the added attributes
+    new_tag = %Q{<#{tag_name}#{attributes}#{id_attribute} data-matomo-content-name="#{matomo_name}">#{content}</#{tag_name}>}.tap { id_counter += 1 }
+    new_tag
   end
 end
