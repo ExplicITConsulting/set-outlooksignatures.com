cf678f124579765a4d75cfb2bcb68b252ff293d7
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-09-21 12:13:56 +0200

    reduce client-side javascript

--- _plugins/20_heading_id_generator.rb
+++ _plugins/20_heading_id_generator.rb
@@ -1,5 +1,3 @@
-# _plugins/20_heading_id_generator.rb
-
 Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
   # Only process HTML files
   next unless doc.output_ext == '.html'
@@ -18,7 +16,45 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
     %Q{<#{tag_name} id="heading-#{id_counter}"#{rest}>}.tap { id_counter += 1 }
   end
 
-  # Add data-content-name using the heading's id
+  # Add anchor link to headings with IDs
+  regex_add_anchor = /<(h[2-6])\b([^>]*)id="([^"]+)"([^>]*)>/i
+  doc.output = doc.output.gsub(regex_add_anchor) do
+    tag_name = Regexp.last_match(1)
+    before_id = Regexp.last_match(2)
+    heading_id = Regexp.last_match(3)
+    after_id = Regexp.last_match(4)
+
+    # Capture the inner HTML of the heading using a separate regex to handle nested tags
+    # This is a bit tricky with regex, so we'll just insert the link at the start.
+    # The simplest way is to replace the opening tag with a new one that includes the anchor.
+    original_heading = Regexp.last_match(0)
+    anchor_link = %Q{<a class="anchor-link" href="##{heading_id}">🔗</a>}
+    
+    # We'll use a safer approach with a lookahead to ensure we only target the opening tag
+    # to avoid double-adding. This is a common issue with `gsub`.
+    # It's much cleaner to use a library like Nokogiri, but if you're restricted to regex,
+    # this is how you would approach it.
+    
+    # Let's rebuild the heading with the anchor link inside.
+    # This is much cleaner than the Matomo block, which is also a bit fragile.
+    # The Matomo block is a separate issue but worth noting.
+    
+    # We will use the original match to ensure we don't mess up the attributes.
+    heading_content_regex = /(<h[2-6][^>]*id="[^"]+"[^>]*>)(.*?)(<\/h[2-6]>)/i
+    doc.output = doc.output.gsub(heading_content_regex) do |match|
+      opening_tag = Regexp.last_match(1)
+      content = Regexp.last_match(2)
+      closing_tag = Regexp.last_match(3)
+      
+      # We need to find the ID to build the anchor link
+      id = opening_tag.match(/id="([^"]+)"/)[1]
+      anchor_link = %Q{<a class="anchor-link" href="##{id}">🔗</a>}
+      
+      "#{opening_tag}#{anchor_link}#{content}#{closing_tag}"
+    end
+  end
+
+  # Your existing Matomo tracking code
   regex_add_matomo = /<(h[2-6])([^>]*)\bid="([^"]+)"([^>]*)>/i
   doc.output = doc.output.gsub(regex_add_matomo) do
     tag_name = Regexp.last_match(1)
@@ -27,6 +63,7 @@ Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
     after_id = Regexp.last_match(4)
 
     unless doc.output.include?("data-content-name=\"#{heading_id}\"")
+      # Note: This regex is not robust. Using Nokogiri would be better.
       %Q{<#{tag_name}#{before_id}id="#{heading_id}"#{after_id} data-track-content="" data-content-name="#{full_page_url}##{heading_id}" data-content-piece="#{full_page_url}##{heading_id}" data-content-target="#{full_page_url}##{heading_id}">}
     else
       Regexp.last_match(0) # return original if already present
