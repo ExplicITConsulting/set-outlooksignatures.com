72f483bfbd839f603c970f1a3bbde339687709be
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-09-24 10:36:29 +0200

    update head-scripts

--- _plugins/link_modifier.rb
+++ _plugins/link_modifier.rb
@@ -6,66 +6,56 @@ module Jekyll
     def modify_links(input)
       site_url = @context.registers[:site].config['url']
       
-      # Use a single regex to capture the entire <a> tag and its href
-      input.gsub(/<a(.*?)href="([^"]+)"(.*?)>/) do |match|
-        pre_href = $1
+      input.gsub(/<a([^>]*?)href="([^"]+)"([^>]*?)>/) do |match|
+        pre_href_attrs = $1
         href = $2
-        post_href = $3
-        
-        # Get the current classes from the tag, if they exist
-        existing_classes = match.scan(/class=["']([^"']*)["']/).flatten.first || ''
-        classes = existing_classes.split
-        
-        # Get the other attributes
-        other_attrs = match.sub(existing_classes.strip, '').sub('class=', '')
-        
+        post_href_attrs = $3
+
         begin
           uri = URI.parse(href)
+
+          # Skip links without a scheme (e.g., #anchors) or special schemes
+          next match unless uri.scheme.nil? || ['http', 'https', 'mailto'].include?(uri.scheme)
+
+          new_attrs = ''
+          new_class = ''
           
-          # Handle mailto and javascript schemes gracefully
-          if ['mailto', 'javascript'].include?(uri.scheme)
-            # Add mtrcs-internal-link for mailto, and leave javascript alone
-            if uri.scheme == 'mailto'
-              classes << 'mtrcs-internal-link' unless classes.include?('mtrcs-internal-link')
-              new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\""
-              next "<a#{pre_href}#{new_attrs}#{post_href}>"
-            end
-            next match
+          # Extract existing classes and other attributes
+          existing_class_match = match.match(/class=["']([^"']*)["']/)
+          if existing_class_match
+            new_class = existing_class_match[1]
           end
-          
+
+          # Check for external vs. internal
           is_external = uri.scheme && uri.hostname && uri.hostname.downcase != URI.parse(site_url).hostname.downcase
 
           if is_external
-            # Add external link class and target
-            classes << 'mtrcs-external-link' unless classes.include?('mtrcs-external-link')
-            new_target = ' target="_blank"' unless match.include?('target="_blank"')
-            
-            # Rebuild the tag with new classes and target
-            new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\"#{new_target}"
+            new_class += " mtrcs-external-link"
+            new_attrs += ' target="_blank"'
             
             # Add the arrow unless the no-external-link-icon class is present
-            if !classes.include?('no-external-link-icon')
-              # Check for the arrow before adding
-              if !match.include?('&nbsp;↗</a>') && !match.include?('↗</a>')
-                new_match = "<a#{pre_href}#{new_attrs}#{post_href}>".sub('</a', '&nbsp;↗</a')
-                next new_match
-              end
+            unless new_class.include?('no-external-link-icon')
+              updated_match = match.sub(/<\/a>/, "&nbsp;↗</a>")
+            else
+              updated_match = match
             end
-            
-            new_match = "<a#{pre_href}#{new_attrs}#{post_href}>"
-            next new_match
           else
-            # Add internal link class
-            classes << 'mtrcs-internal-link' unless classes.include?('mtrcs-internal-link')
-            
-            # Rebuild the tag with new classes
-            new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\""
-            new_match = "<a#{pre_href}#{new_attrs}#{post_href}>"
-            next new_match
+            new_class += " mtrcs-internal-link"
+            updated_match = match
           end
+
+          # Rebuild the tag with new classes and attributes
+          updated_match = updated_match.sub(/class=["']([^"']*)["']/, '').sub("href=\"#{href}\"", "href=\"#{href}\"#{new_attrs}")
+          
+          unless new_class.strip.empty?
+            updated_match = updated_match.sub("href=\"#{href}\"", "href=\"#{href}\" class=\"#{new_class.strip}\"")
+          end
+          
+          updated_match
+
         rescue URI::InvalidURIError
-          # Invalid URI, return the original match
-          next match
+          # Catches any and all invalid URI formats, including $CurrentUserMail$
+          match
         end
       end
     end
