bc504ee830306480fdbb8c9587ee7c8b46fdf79a
Markus Gruber <26838358+GruberMarkus@users.noreply.github.com>
2025-09-24 10:31:59 +0200

    update head-scripts

--- _plugins/link_modifier.rb
+++ _plugins/link_modifier.rb
@@ -4,46 +4,72 @@ require 'uri'
 module Jekyll
   module LinkModifier
     def modify_links(input)
-      # Get the current site URL from the Jekyll context
       site_url = @context.registers[:site].config['url']
       
-      # Use a regular expression to find all <a> tags
-      input.gsub(/<a[^>]*href="([^"]+)"[^>]*>/) do |match|
-        href = $1
+      # Use a single regex to capture the entire <a> tag and its href
+      input.gsub(/<a(.*?)href="([^"]+)"(.*?)>/) do |match|
+        pre_href = $1
+        href = $2
+        post_href = $3
         
-        # Parse the href to check if it's an external link
+        # Get the current classes from the tag, if they exist
+        existing_classes = match.scan(/class=["']([^"']*)["']/).flatten.first || ''
+        classes = existing_classes.split
+        
+        # Get the other attributes
+        other_attrs = match.sub(existing_classes.strip, '').sub('class=', '')
+        
         begin
           uri = URI.parse(href)
           
-          # Check if the href is a valid URI with a hostname
-          if uri.scheme && uri.hostname
-            # Check if the hostname is different from the site's hostname
-            if uri.hostname.downcase != URI.parse(site_url).hostname.downcase
-              # Add target="_blank" and the mtrcs-external-link class
-              updated_match = match.sub("href=\"#{href}\"", "href=\"#{href}\" target=\"_blank\" class=\"mtrcs-external-link\"")
-              
-              # Add the arrow unless the no-external-link-icon class is present
-              unless updated_match.include?('class="no-external-link-icon"')
-                updated_match.sub(/<\/a>/, "&nbsp;↗</a>")
-              else
-                updated_match
-              end
-            else
-              # Add the mtrcs-internal-link class for internal links
-              match.sub("href=\"#{href}\"", "href=\"#{href}\" class=\"mtrcs-internal-link\"")
+          # Handle mailto and javascript schemes gracefully
+          if ['mailto', 'javascript'].include?(uri.scheme)
+            # Add mtrcs-internal-link for mailto, and leave javascript alone
+            if uri.scheme == 'mailto'
+              classes << 'mtrcs-internal-link' unless classes.include?('mtrcs-internal-link')
+              new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\""
+              next "<a#{pre_href}#{new_attrs}#{post_href}>"
             end
+            next match
+          end
+          
+          is_external = uri.scheme && uri.hostname && uri.hostname.downcase != URI.parse(site_url).hostname.downcase
+
+          if is_external
+            # Add external link class and target
+            classes << 'mtrcs-external-link' unless classes.include?('mtrcs-external-link')
+            new_target = ' target="_blank"' unless match.include?('target="_blank"')
+            
+            # Rebuild the tag with new classes and target
+            new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\"#{new_target}"
+            
+            # Add the arrow unless the no-external-link-icon class is present
+            if !classes.include?('no-external-link-icon')
+              # Check for the arrow before adding
+              if !match.include?('&nbsp;↗</a>') && !match.include?('↗</a>')
+                new_match = "<a#{pre_href}#{new_attrs}#{post_href}>".sub('</a', '&nbsp;↗</a')
+                next new_match
+              end
+            end
+            
+            new_match = "<a#{pre_href}#{new_attrs}#{post_href}>"
+            next new_match
           else
-            # For relative links, add the mtrcs-internal-link class
-            match.sub("href=\"#{href}\"", "href=\"#{href}\" class=\"mtrcs-internal-link\"")
+            # Add internal link class
+            classes << 'mtrcs-internal-link' unless classes.include?('mtrcs-internal-link')
+            
+            # Rebuild the tag with new classes
+            new_attrs = "href=\"#{href}\" class=\"#{classes.join(' ')}\""
+            new_match = "<a#{pre_href}#{new_attrs}#{post_href}>"
+            next new_match
           end
         rescue URI::InvalidURIError
-          # If it's an invalid URL, return the original link
-          match
+          # Invalid URI, return the original match
+          next match
         end
       end
     end
   end
 end
 
-# Register the filter with Jekyll
 Liquid::Template.register_filter(Jekyll::LinkModifier)
